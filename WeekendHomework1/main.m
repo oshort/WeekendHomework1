6dc8b5ee5cc44b7d91d665e7fd5be740ef9e8d24
Oliver Short <oshort55@gmail.com>
2016-04-14 13:18:48 -0400

    Initial Commit

---
+++ WeekendHomework1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WeekendHomework1
+//
+//  Created by Oliver Short on 4/14/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
